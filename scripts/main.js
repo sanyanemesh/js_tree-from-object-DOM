9536a3b71200f75bbe69402196260564873a1b43
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-15 14:07:39 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const ul = document.createElement('ul');
+
+  element.append(ul);
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    ul.append(li);
+    li.textContent = key;
+
+    if (typeof data[key] === 'object') {
+      createTree(li, data[key]);
+    }
+  }
+}
+
+createTree(tree, food);
